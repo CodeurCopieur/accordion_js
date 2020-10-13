929b99e01ac62f9992633f3fa761e32617b48867
codwerk <louis_widdy@live.fr>
2020-10-13 23:22:41 +0200

    intégration html/css/js

---
+++ index.js
@@ -0,0 +1,47 @@
+let faqs = [
+  {
+    question: 'One Piece vs DBZ ?',
+    answer: 'DBZ'
+  },
+  {
+    question: 'Naruto vs One Piece ?',
+    answer: 'Naruto'
+  },
+  {
+    question: 'Marvel vs DC Comics ?',
+    answer: 'Marvel'
+  }
+];
+
+const faqs_element = document.querySelector('.faqs');
+
+function displayList(array = []) {
+
+  faqs_element.innerHTML= "";
+
+  for (let index = 0; index < array.length; index++) {
+    const faq = array[index];
+    
+
+    const template = `<div class="faq">
+    <div class="question">${faq.question}</div>
+    <div class="answer">${faq.answer}</div>
+    </div>`;
+
+    faqs_element.insertAdjacentHTML('beforeend', template);
+  }
+}
+
+displayList(faqs);
+
+
+const faq_element = document.querySelectorAll('.faq');
+
+function linkAction(e){
+  faq_element.forEach( n => n.classList.remove('open'));
+  
+  this.classList.toggle('open');
+
+}
+
+faq_element.forEach( faq => faq.addEventListener('click', linkAction))
